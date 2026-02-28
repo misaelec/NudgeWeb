57b037ef283a11a46245eecd4201b6c36a74d1e0
Misael Espinosa <misael@MacBook-Air-de-Misael.local>
2026-02-28 13:50:49 -0600

    Syncing.

--- lib/calendar/webhookManager.ts
+++ lib/calendar/webhookManager.ts
@@ -23,7 +23,7 @@ export async function registerWebhook(calendarDbId: string, accessToken: string,
 
   try {
     const response = await fetch(
-      `https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(googleCalendarId)}/watch`,
+      `https://www.googleapis.com/calendar/v3/calendars/${encodeURIComponent(googleCalendarId)}/events/watch`,
       {
         method: 'POST',
         headers: {
